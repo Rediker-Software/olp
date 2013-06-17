70a7558d3e5af35e36f7c0c237861d690566625a
Kevin Brown <bass5098@gmail.com>
2013-06-17 19:01:38 -0400

    Added two more tests for no permissions

--- tests/tests/backend.py
+++ tests/tests/backend.py
@@ -1,19 +1,37 @@
 from django.test import TestCase
 from olp.backends import PermissionBackend
+from ..models import Apple
 
 
 class TestBackend(TestCase):
 
-    def test_no_authenticate(self):
-        backend = PermissionBackend()
+    def setUp(self):
+        from django.contrib.auth.models import User
 
-        result = backend.authenticate()
+        self.user = User.objects.create_user("test", "test@test.com", "test")
+        self.user.save()
+
+        self.backend = PermissionBackend()
+
+    def test_no_authenticate(self):
+        result = self.backend.authenticate()
 
         self.assertEqual(result, None)
 
     def test_get_user(self):
-        backend = PermissionBackend()
-
-        result = backend.get_user()
+        result = self.backend.get_user()
 
         self.assertEqual(result, None)
+
+    def test_user_no_permissions(self):
+        result = self.backend.get_all_permissions(self.user)
+
+        self.assertEqual(result, set())
+
+    def test_user_no_obj_permissions(self):
+        apple = Apple(name="test")
+        apple.save()
+
+        result = self.backend.get_all_permissions(self.user, apple)
+
+        self.assertEqual(result, set())
