4bd1bb2dc55d2238326810c0311e485adee26dcd
Kevin Brown <bass5098@gmail.com>
2013-06-17 19:36:30 -0400

    Started working on User.assign_perm

--- olp/utils.py
+++ olp/utils.py
@@ -1,5 +1,27 @@
-def assign_perm(self, permission, obj=None):
-    pass
+def assign_perm(user, permission, obj=None):
+    """
+    Assign a permission to a user
+    """
+
+    from django.contrib.auth.models import Permission
+
+    # Check if the given permission is a Permission object
+    # If the permission is given as a string, get the corresponding Permission object
+
+    if not hasattr(permission, "pk"):
+        app_label, codename = permission.aplit(".")
+
+        permissions = Permission.objects.filter(codename=codename)
+
+        if len(permissions) != 1:
+            permission = permissions.get(content_type__app_label=app_label)
+        else:
+            permission = permissions.get()
+
+    if obj:
+        pass
+    else:
+        user.user_permissions.add(permission)
 
 
 def patch_user():
--- tests/tests/__init__.py
+++ tests/tests/__init__.py
@@ -1,4 +1,5 @@
 from olp.utils import patch_user
 from .backend import TestBackendBasic
+from .utils import TestAssignPerm
 
 patch_user()
