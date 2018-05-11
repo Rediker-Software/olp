69371be4d9dc41bc1fe1b437edc74b9085438c33
Kevin Brown <kevin@kevin-brown.com>
2018-05-10 21:52:31 -0400

    Pin pytest-django to 3.1
    This is so we can support back to Django 1.7, as well as Django 1.11.
    We can probably drop Django 1.7 at this point, since it's not
    actually supported, but that's not nearly as fun as maintaining
    backwards compatibility.

--- tests/tests/__init__.py
+++ tests/tests/__init__.py
@@ -1,6 +1,3 @@
 from olp.utils import patch_models
-from .backend import TestBackendBasic
-from .utils import TestAssignPerm, TestHasPerm, TestRemovePerm, \
-    TestRemoveAllPermissions, TestRemovePermNotSet, TestGetObjsForUser
 
 patch_models()
