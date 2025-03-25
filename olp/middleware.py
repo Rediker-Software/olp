a59ac9afe4afbe01db24cfb5eafc4b0b2083e7e1
Chandrasheshekartnous <105643659+Chandrasheshekartnous@users.noreply.github.com>
2025-03-25 20:27:27 +0530

    Update middleware.py

--- olp/middleware.py
+++ olp/middleware.py
@@ -1,9 +1,31 @@
-class PatchModelsMiddleware(object):
+# class PatchModelsMiddleware(object):
 
-    def __init__(self):
-        from django.core.exceptions import MiddlewareNotUsed
-        from .utils import patch_models
+#     def __init__(self):
+#         from django.core.exceptions import MiddlewareNotUsed
+#         from .utils import patch_models
 
+#         patch_models()
+
+#         raise MiddlewareNotUsed()
+
+from django.core.exceptions import MiddlewareNotUsed
+from .utils import patch_models
+
+
+class PatchModelsMiddleware:
+    def __init__(self, get_response):
+        # Accepting the get_response parameter as required by Django
+        self.get_response = get_response
+
+        # Call the patch_models function
         patch_models()
 
-        raise MiddlewareNotUsed()
+        # Optionally raise MiddlewareNotUsed to stop further processing
+        raise MiddlewareNotUsed("PatchModelsMiddleware has been used")
+
+    def __call__(self, request):
+        # This is where the actual middleware chain happens.
+        # The middleware does not process the request any further
+        # due to the `MiddlewareNotUsed` raised in __init__.
+        return self.get_response(request)
+
